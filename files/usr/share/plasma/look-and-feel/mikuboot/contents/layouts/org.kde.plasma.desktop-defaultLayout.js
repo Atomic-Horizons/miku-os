f7a33dc48bb8c4db965d05bc1e3723095a19fb6c
Lisan <69053985+shinylisan@users.noreply.github.com>
2026-02-23 17:59:59 -0300

    Update org.kde.plasma.desktop-defaultLayout.js

--- files/usr/share/plasma/look-and-feel/mikuboot/contents/layouts/org.kde.plasma.desktop-defaultLayout.js
+++ files/usr/share/plasma/look-and-feel/mikuboot/contents/layouts/org.kde.plasma.desktop-defaultLayout.js
@@ -1,24 +1,22 @@
 var panel = new Panel
 panel.location = "bottom"
-panel.height = 44
+panel.height = 44  // This is your magic number
 
-// This creates the widget list
+// This defines which widgets appear on your MikuOS panel
 var widgets = [
-    "org.kde.plasma.kickoff", // This is the Start Menu
-    "org.kde.plasma.pager",
-    "org.kde.plasma.icontasks",
+    "org.kde.plasma.kickoff",      // Start Menu
+    "org.kde.plasma.pager",        // Desktop Cube/Pager
+    "org.kde.plasma.icontasks",    // Task Manager (Icons only)
     "org.kde.plasma.marginsseparator",
-    "org.kde.plasma.systemtray",
-    "org.kde.plasma.digitalclock"
+    "org.kde.plasma.systemtray",   // System Tray (Wifi, Volume, etc)
 ]
 
-panel.newlines = []
 for (var i = 0; i < widgets.length; i++) {
     var widget = panel.addWidget(widgets[i])
     
-    // If the widget we just added is the Start Menu (Kickoff), change the icon
+    // Ensure the Start Menu uses your custom icon name
     if (widgets[i] === "org.kde.plasma.kickoff") {
         widget.currentConfigGroup = ["General"]
-        widget.writeConfig("icon", "start-here")
+        widget.writeConfig("icon", "start-here-kde-symbolic")
     }
 }
